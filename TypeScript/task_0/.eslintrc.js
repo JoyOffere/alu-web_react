24a074b856bb517664d88a324b28bb1684d9827f
Joy Offere <113602216+JoyOffere@users.noreply.github.com>
2025-06-15 19:42:03 +0200

    feat: add ESLint configuration and initial TypeScript setup with tsconfig and webpack files

---
+++ TypeScript/task_0/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['plugin:@typescript-eslint/recommended'],
+  rules: {
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off'
+  }
+};
