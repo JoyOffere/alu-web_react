ac6348742835670419c51ed8f6cce9b6e2584206
Joy Offere <113602216+JoyOffere@users.noreply.github.com>
2025-06-16 23:48:34 +0200

    Updated task 3

---
+++ react_redux_connectors_and_providers/redux-connectors/task_3/dashboard/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from '@redux-devtools/extension';
+import rootReducer from './dashboard/src/App/reducers/rootReducer';
+import App from './dashboard/src/App/App';
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
